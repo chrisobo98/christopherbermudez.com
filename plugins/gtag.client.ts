4046f9fcf47f9df97c6e0ffbee107387f83b1d4c
chrisobo98 <christopherbermudez98@gmail.com>
2024-12-19 11:24:25 -0500

    feat: remove cookie requirements from analytics

--- plugins/gtag.client.ts
+++ plugins/gtag.client.ts
@@ -1,6 +1,6 @@
 // plugins/gtag.client.ts
-import { defineNuxtPlugin } from '#app'
-import { useRuntimeConfig, useHead } from '#imports'
+import { defineNuxtPlugin } from "#app";
+import { useRuntimeConfig, useHead } from "#imports";
 
 declare global {
   interface Window {
@@ -9,22 +9,21 @@ declare global {
 }
 
 export default defineNuxtPlugin(() => {
-  const cookieControl = useCookieControl()
-  const { gtagId } = useRuntimeConfig().public
+  const { gtagId } = useRuntimeConfig().public;
 
-  if (cookieControl.cookiesEnabledIds?.value?.includes('google-analytics')) {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(...args: any[]) { window.dataLayer.push(args); }
-    gtag('js', new Date());
-    gtag('config', gtagId);
-        
-    useHead({
-      script: [
-        {
-          src: `https://www.googletagmanager.com/gtag/js?id=${gtagId}`,
-          async: true,
-        },
-      ],
-    });
+  window.dataLayer = window.dataLayer || [];
+  function gtag(...args: any[]) {
+    window.dataLayer.push(args);
   }
+  gtag("js", new Date());
+  gtag("config", gtagId);
+
+  useHead({
+    script: [
+      {
+        src: `https://www.googletagmanager.com/gtag/js?id=${gtagId}`,
+        async: true,
+      },
+    ],
+  });
 });
