64ff16fe565aa293ac526b9833d0315b7bc5e51a
chrisobo98 <christopherbermudez98@gmail.com>
2025-01-01 17:38:52 -0500

    feat: seo updates

--- app.vue
+++ app.vue
@@ -5,23 +5,24 @@
 
 <script setup lang="ts">
 useHead({
-  title: "Christopher Bermudez - Web & Software Developer in Orlando, FL",
+  title: "Christopher Bermudez - Web Developer & SEO Expert in Orlando & Clermont, FL",
   meta: [
     { charset: "utf-8" },
     { name: "viewport", content: "width=device-width, initial-scale=1" },
     {
       hid: "description",
       name: "description",
-      content: "Christopher Bermudez here—an Orlando web developer and SEO expert crafting custom websites and boosting local businesses’ online presence. Let’s work together!",
+      content: "I’m Christopher Bermudez—an experienced web developer and SEO expert based in Orlando and Clermont, FL. I specialize in creating custom websites and helping local businesses grow their online presence. Let’s collaborate to build something amazing!",
     },
     {
       name: "keywords",
-      content: "Web Development Orlando, SEO Expert Orlando, Custom Websites Florida, Central Florida Web Developer, Local SEO Services Orlando, Small Business Website Design, Digital Marketing Central Florida, Tailored Web Solutions Orlando, E-commerce Development Orlando, Responsive Website Design Florida, Affordable web development for Orlando small businesses, Improve website rankings with local SEO services, Custom software solutions for Central Florida companies",
+      content: "Web Developer Clermont, SEO Expert Orlando, Custom Websites Florida, Orlando Web Design, Local SEO Services Clermont, Small Business Website Design Orlando, Digital Marketing Central Florida, Tailored Web Solutions Orlando, E-commerce Development Florida, Responsive Website Design Clermont, Affordable Web Development Orlando, Local SEO Expert, Custom Software Solutions Central Florida",
     },
     { name: "author", content: "Christopher Bermudez" },
   ],
 });
 </script>
+
 
 <style scoped>
 /* Add any global CSS styles here */
